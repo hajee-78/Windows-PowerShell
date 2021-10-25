ed432faa84b722a81c05e29707be40c06176fc54
hajee-78 <55215524+hajee-78@users.noreply.github.com>
2021-10-25 20:35:07 +0800

    Update Array.ps1

--- PowerShell basics/Simple scripts/Array.ps1
+++ PowerShell basics/Simple scripts/Array.ps1
@@ -1,1 +1,26 @@
+#Declaring array of numbers 
+$myarray=14..8
+
+#To view the output
+PS C:\WINDOWS\system32> $myarray
+14
+13
+12
+11
+10
+9
+8
+
+#To confirm array value
+PS C:\WINDOWS\system32> $myarray.contains(10)
+True
+
+#To measure the length of array
+PS C:\WINDOWS\system32> $myarray.Length
+7
+
+#To count the numbers in array
+PS C:\WINDOWS\system32> $myarray.Count
+7
+
 
