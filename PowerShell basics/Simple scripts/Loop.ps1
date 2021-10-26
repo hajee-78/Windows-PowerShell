2e044bdbb90001c1ea7810a1e230045709e73f78
hajee-78 <55215524+hajee-78@users.noreply.github.com>
2021-10-26 20:38:00 +0800

    Update Loop.ps1

--- PowerShell basics/Simple scripts/Loop.ps1
+++ PowerShell basics/Simple scripts/Loop.ps1
@@ -1,1 +1,27 @@
+#Foreach loop example
+$values = 1..10
+foreach ($number in $values)
+{ Write-Host $number }
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
 
+#For loop example
+for($i=1; $i -lt 10; $i++)
+{Write-Host $i}
+1
+2
+3
+4
+5
+6
+7
+8
+9
