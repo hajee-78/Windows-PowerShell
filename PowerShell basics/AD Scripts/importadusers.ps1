03836f393a5f8f0ecd55a6b7e26762ae52eb6636
hajee-78 <55215524+hajee-78@users.noreply.github.com>
2021-10-27 21:00:58 +0800

    Add files via upload

---
+++ PowerShell basics/AD Scripts/importadusers.ps1
@@ -0,0 +1,16 @@
+﻿Function import-hhhnetusers {
+    param(
+        [String] $OUName)
+
+    $Import = "C:\PSScript\"+$OUName+".csv"
+    $Location = "OU="+$OUName+",DC=hhhnet,DC=local"
+
+    New-ADOrganizationalUnit -Name $OUName -path "DC=hhhnet,DC=local" -ProtectedFromAccidentalDeletion $false
+    $Password = "_L0c@ladMi5" | ConvertTo-SecureString -AsPlainText -Force
+    Import-Csv $Import | New-ADUser -Path $Location -AccountPassword $Password -Enabled $true
+    }
+
+    import-hhhnetusers -OUName HR
+    import-hhhnetusers -OUName IT
+    import-hhhnetusers -OUName Marketing
+    import-hhhnetusers -OUName Sales
