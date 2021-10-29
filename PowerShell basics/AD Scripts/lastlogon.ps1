c89ca0add89baf238d985bb27e2f4771871244dd
hajee-78 <55215524+hajee-78@users.noreply.github.com>
2021-10-29 11:22:46 +0800

    Update lastlogon.ps1

--- PowerShell basics/AD Scripts/lastlogon.ps1
+++ PowerShell basics/AD Scripts/lastlogon.ps1
@@ -1,1 +1,4 @@
- Get-ADUser -Filter {enabled -eq $true} -Properties lastlogon -SearchBase "OU=xxx,OU=xxx,DC=xxx,DC=xxx,DC=xxx" | Select-Object Name, lastlogon
+#To find last logon users / service accounts in an AD
+#Replace xxx with your organization values. For Example "OU=users,DC=Contoso,DC=Com"
+Get-ADUser -Filter {enabled -eq $true} -Properties lastlogon -SearchBase "OU=xxx,DC=xxx,DC=xxx" | Select-Object Name, lastlogon
+#The output of the above command 
